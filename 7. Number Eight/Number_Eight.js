e24ef4c13922d4dfb6e248ec3a2831b9d81a3958
BabarBurhan0 <160392922+BabarBurhan0@users.noreply.github.com>
2024-08-04 17:25:43 +0500

    Add files via upload

---
+++ 7. Number Eight/Number_Eight.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log(6 + 2); // addition method by me
+console.log(10 - 2); // Subtraction method
+console.log(4 * 2); // multiplication
+console.log(16 / 2); // divide
---
+++ 7. Number Eight/Number_Eight.ts
@@ -0,0 +1,9 @@
+
+
+console.log(6+2);  // addition method by me
+
+console.log(10-2); // Subtraction method
+
+console.log(4*2); // multiplication
+
+console.log(16/2); // divide
